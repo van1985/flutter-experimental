dd618510e94264dbfac58aac9b900a84ee95556a
leandro.garcia <leandro.garcia@globant.com>
2019-08-08 22:31:19 -0300

    Add flutter mobile & web

---
+++ hummingbird_mobile/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/leandro.garcia/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/leandro.garcia/Documents/Globant/Flutter/flutter-experimental/hummingbird_mobile"
+export "FLUTTER_TARGET=/Users/leandro.garcia/Documents/Globant/Flutter/flutter-experimental/hummingbird_mobile/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/leandro.garcia/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
